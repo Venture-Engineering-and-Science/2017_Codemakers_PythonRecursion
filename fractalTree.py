2a1e6aa3841f2c05e324911cc1ee1732f283c51a
Andrew Berriault <berriaak@mcmaster.ca>
2017-07-16 15:40:08 -0400

    Create fractalTree.py

---
+++ fractalTree.py
@@ -0,0 +1,49 @@
+# Filename: fractalTree
+# Camp: Venture Codemakers Advanced
+# Date: Summer 2017
+# Created by: Anna
+
+import turtle
+
+
+def tree(branchLen,t):
+
+    if branchLen > 5:
+
+        t.forward(branchLen)
+
+        t.right(20)
+
+        tree(branchLen-15,t)
+
+        t.left(40)
+
+        tree(branchLen-15,t)
+
+        t.right(20)
+
+        t.backward(branchLen)
+
+
+def main():
+
+    t = turtle.Turtle()
+
+    myWin = turtle.Screen()
+
+    t.left(90)
+
+    t.up()
+
+    t.backward(100)
+
+    t.down()
+
+    t.color("green")
+
+    tree(50,t)
+
+    myWin.exitonclick()
+
+
+main()
